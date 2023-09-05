44d0ac8274b4e5b89c682d6ff64f531dc0868182
Luxor5k <santinolede3@gmail.com>
2023-09-05 19:57:13 -0300

    Test

---
+++ Python/Clase3.py
@@ -0,0 +1,7 @@
+
+SeleccionArgentina = {
+    10 : {"Nombre": "Lionel Messi", "Edad": 36, "Altura": 1.69, "Precio": 35000000},
+}
+
+for llave, valor in SeleccionArgentina.items():
+    print(llave, valor)
