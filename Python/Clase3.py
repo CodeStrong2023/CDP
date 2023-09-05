7a641d85c9b3584a3979cff25589f6b18fb8ec06
Luxor5k <santinolede3@gmail.com>
2023-09-05 20:04:28 -0300

    fix: code

--- Python/Clase3.py
+++ Python/Clase3.py
@@ -1,7 +1,7 @@
-
-SeleccionArgentina = {
+seleccionArgentina = {
     10 : {"Nombre": "Lionel Messi", "Edad": 36, "Altura": 1.69, "Precio": 35000000},
 }
 
-for llave, valor in SeleccionArgentina.items():
-    print(llave, valor)
+seleccionArgentina[8] = {"Nombre": "Rodrigo De Paul", "Edad": 28, "Altura": 1.76, "Precio": 2000000}
+
+print(seleccionArgentina)
